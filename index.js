a031a5ac5daadba8162decae4d974e2b4cf9a383
iamchacha021 <iamchacha021@gmail.com>
2022-11-28 11:41:22 +0300

    Completed this assignment after one week

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    
+    
+    if (feet <= 400){
+     return 'This one is on me!';
+    }
+    if(feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.';
+    }
+    else {
+    return  'No can do.';
+    }
+    
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';  
 }
+ternaryCheckCity('Pittsburgh')
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  // const tip = 'generous';
+  // let reply;
+ switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+     
+    
+      case 'not as generous':
+        return'Thank you.'
+        
+
+      default:
+        return 'Bye.'
+ }
+  
+}
+switchOnCharmFromTip('generous')
+switchOnCharmFromTip('not as generous')
+switchOnCharmFromTip('thanks for everything')
